17c8912bb59652018c051d3ba6170c3d66f74b3d
Hamza Tayeb <hamza.tayeb@gmail.com>
2017-09-16 09:36:26 -0700

    basically working rspec
    will fail when the token gets old… need to solve that

--- spec/google-auth_token_validator_spec.rb
+++ spec/google-auth_token_validator_spec.rb
@@ -1,11 +1,14 @@
 require "spec_helper"
 
 RSpec.describe Google::Auth::TokenValidator do
+  include_context "test data"
+
   it "has a version number" do
     expect(Google::Auth::TokenValidator::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "validates a token" do
+    validator = Google::Auth::TokenValidator.new data[:id_token][:good], data[:client_id]
+    expect(validator.validate).to eq(true)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
 require "bundler/setup"
+require "yaml"
+require "json"
 require "googleauth/token_validator"
+
+RSpec.shared_context "test data", :shared_context => :metadata do
+  yaml = YAML.load_file File.join(__dir__, "data.yml")
+  let(:data) { JSON.parse(yaml.to_json, symbolize_names: true) }
+end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -11,4 +18,6 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.include_context "test data", :include_shared => true
 end
